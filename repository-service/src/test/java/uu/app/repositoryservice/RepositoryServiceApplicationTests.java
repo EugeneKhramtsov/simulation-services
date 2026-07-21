3269b3a03a396a7306110975e112feb45a1e76b1
EugeneKhramtsov <yevheniikhramtsov@gmail.com>
2026-07-21 13:11:29 +0300

    Initial commit

---
+++ repository-service/src/test/java/uu/app/repositoryservice/RepositoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uu.app.repositoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
